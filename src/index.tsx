99e08ec939478cd75403f1d4c9d16ca6d36c6bfd
partha <parthakhanra.tal@gmail.com>
2022-08-01 16:16:34 +0530

    html theme to react theme convert

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Application from './component/Application';
-import Checkbox from './component/checkbox';
-import CalenderSettingsModal from './component/checkkey';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Application />
-
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
